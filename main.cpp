7e4c9530b64beb6fa558bd3ea798c4da02b53207
Bob (SemenchenkoND) <bob@example.com>
2022-04-02 17:55:40 +0300

    code: добавил умножение

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main()
 	int a, b;
 	cin >> a >> b;
 	cout << "A + B = " << a + b << endl
-		<< "A - B = " << a - b << endl;
+		<< "A - B = " << a - b << endl
+		<< "A * B = " << a * b << '\n';
 
 	return 0;
 }
